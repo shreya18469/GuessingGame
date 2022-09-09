268a0c9c43468ad01c5de8b0187d51fc24f2fd07
Shreya Suresh <shreya4763@gmail.com>
2022-09-09 13:15:44 -0700

    Num generator and too high/low prompts finished

--- GuessingGame.cpp
+++ GuessingGame.cpp
@@ -1,12 +1,30 @@
 #include <iostream>
-
+#include <ctime>
 using namespace std;
 
 int main()
 {
   int input;
-  cin >> input;
+  int num;
+  int tries = 0;
+  srand(time(0));
+  num = rand() % 100 +1;
+  cout << "Guess a number 1-100";
+
+  do {
+    cin >> input;
+    tries++;
+    if (input > num)
+      cout << "Too high ";
+    else if (input < num)
+      cout << "Too low ";
+    else{
+      cout << "Correct! You got it in " << tries << " guesses";
+    }
+  } while (input != num);
+
   
-  cout << input;
+
   return 0;
+
 }
